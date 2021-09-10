6c3a9f5b5f82aba77434b9cc4636d4e68c566fbc
Maksim <maks.kushnirov@gmail.com>
2021-09-11 02:12:33 +0300

    коллизия и гравитация (игрок стоит на земле, падает в воздухе)

--- world.py
+++ world.py
@@ -2,11 +2,21 @@ import pygame as pg
 
 
 class Physics:
+
     G = 10
-    def __init__(self, objects: dict):
-        self.objects = objects
+
+    def __init__(self, game):
+        self.game = game
+        self.objects = game.physics_group
+        self.last_time = pg.time.get_ticks()
+        self.acceleration = 60
+
 
     def gravity(self):
         for object in self.objects:
-            object.rect.y += Physics.G
-
+            if pg.sprite.spritecollide(object, self.game.static_group, False):
+                object.falltime = 0
+                self.acceleration = 0
+                object.on_ground = True
+            else: self.acceleration = 60
+            object.rect.y += Physics.G*self.acceleration*self.game.dt
